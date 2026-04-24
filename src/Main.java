6af021463b2cf536d52fc66b2b675d0bbde6ddb4
esteban-ai24 <estebanf2024@gmail.com>
2026-04-24 08:21:26 -0500

    Updated for loop.

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,8 @@ public class Main{
     public String main(String[] args) {
 
         for(int i = 0; i < 3; i++){
-            System.out.println(i);
+            System.out.print(i);
+            System.out.println(": change._.");
         }
         System.out.println("Hello World!");
 
